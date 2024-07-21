7730c7b4eb660420e64347d50076cd2d39e91ad4
sausissimo <pinchermaria@gmail.com>
2024-07-21 16:37:00 +0600

    Completed two of fifteen tasks.

---
+++ CreateDataFrameFromList.py
@@ -0,0 +1,8 @@
+﻿import pandas as pd
+
+def createDataFrame(studentData: list[list[int]]):
+    print(pd.DataFrame(studentData, columns = ["student_id", "age"]))
+
+studentData = [[1,15],[2,11],[3,11],[4,20]]
+
+createDataFrame(studentData)
---
+++ GetSizeOfDataFrame.py
@@ -0,0 +1,12 @@
+﻿import pandas as pd
+import numpy
+
+def getDataframeSize(players: pd.DataFrame) -> list[int]:
+    return players.shape
+
+players = [[1,15,2],[2,11,2],[3,11,2],[4,20,2]]
+playersDF = pd.DataFrame(players, columns = ["name", "type", "aboba"])
+print(playersDF)
+
+print()
+print(numpy.array(getDataframeSize(playersDF)))
